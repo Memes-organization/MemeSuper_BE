b9d9e09ab60d60d3f438ad984bcf6f77d728816e
Huy Thai <huythaia123@gmail.com>
2023-12-31 18:24:51 +0700

    catch error download file

--- src/controllers/memeController.js
+++ src/controllers/memeController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 import { Meme } from '@src/schemas/memeSchema'
 import { CreatedSuccess, OKSuccess } from '@src/helpers/SuccessResponse'
 import { getPathMemeImg, pickKeys } from '@src/utils'
+import { NotFoundException } from '@src/helpers/ErrorResponse'
 
 export const uploadMeme = async (req, res, next) => {
   // const { fieldname, mimetype, filename, size } = req.file
@@ -24,6 +25,9 @@ export const getListMeme = async (req, res, next) => {
   res.status(StatusCodes.OK).json(new OKSuccess('', { ...listMeme }))
 }
 
-export const downloadMeme = (req, res, next) => {
-  res.download(getPathMemeImg(req.params.filename))
+export const downloadMeme = async (req, res, next) => {
+  const { filename } = req.params
+  return res.download(getPathMemeImg(filename), (err) => {
+    if (err) next(new NotFoundException(`File ${filename} not found`))
+  })
 }
