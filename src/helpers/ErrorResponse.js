3cd6e94d5ddb9cff829ade3568015ae48981b965
Huy Thai <huythaia123@gmail.com>
2024-01-02 23:10:01 +0700

    modified ErrorResponse and SuccessResponse

--- src/helpers/ErrorResponse.js
+++ src/helpers/ErrorResponse.js
@@ -6,50 +6,43 @@ export class ErrorResponse extends Error {
     this.name = 'ErrorResponse'
     this.statusCode = statusCode
     this.msg = message
-    this.options = options instanceof Object && Object.keys(options) > 0 ? options : undefined
+    this.options =
+      options instanceof Object && Object.keys(options).length > 0 ? options : undefined
     Error.captureStackTrace(this, this.constructor)
   }
 }
 
-export class ConflictException extends ErrorResponse {
-  constructor(message = ReasonPhrases.CONFLICT, options = {}, status = StatusCodes.CONFLICT) {
-    super(message, status, options)
-    this.name = 'ConflictException'
+export class BadRequestException extends ErrorResponse {
+  constructor(message = ReasonPhrases.BAD_REQUEST, options = {}) {
+    super(message, StatusCodes.BAD_REQUEST, options)
+    this.name = 'BadRequestException'
   }
 }
 
 export class UnauthorizedException extends ErrorResponse {
-  constructor(
-    message = ReasonPhrases.UNAUTHORIZED,
-    options = {},
-    status = StatusCodes.UNAUTHORIZED,
-  ) {
-    super(message, status, options)
+  constructor(message = ReasonPhrases.UNAUTHORIZED, options = {}) {
+    super(message, StatusCodes.UNAUTHORIZED, options)
     this.name = 'UnauthorizedException'
   }
 }
 
 export class NotFoundException extends ErrorResponse {
-  constructor(message = ReasonPhrases.NOT_FOUND, options = {}, status = StatusCodes.NOT_FOUND) {
-    super(message, status, options)
+  constructor(message = ReasonPhrases.NOT_FOUND, options = {}) {
+    super(message, StatusCodes.NOT_FOUND, options)
     this.name = 'NotFoundException'
   }
 }
 
-export class UnprocessableEntityException extends ErrorResponse {
-  constructor(
-    message = ReasonPhrases.UNPROCESSABLE_ENTITY,
-    options = {},
-    status = StatusCodes.UNPROCESSABLE_ENTITY,
-  ) {
-    super(message, status, options)
-    this.name = 'UnprocessableEntityException'
+export class ConflictException extends ErrorResponse {
+  constructor(message = ReasonPhrases.CONFLICT, options = {}) {
+    super(message, StatusCodes.CONFLICT, options)
+    this.name = 'ConflictException'
   }
 }
 
-export class BadRequestException extends ErrorResponse {
-  constructor(message = ReasonPhrases.BAD_REQUEST, options = {}, status = StatusCodes.BAD_REQUEST) {
-    super(message, status, options)
-    this.name = 'BadRequestException'
+export class UnprocessableEntityException extends ErrorResponse {
+  constructor(message = ReasonPhrases.UNPROCESSABLE_ENTITY, options = {}) {
+    super(message, StatusCodes.UNPROCESSABLE_ENTITY, options)
+    this.name = 'UnprocessableEntityException'
   }
 }
