0005c238e0a0a984c221eb2bf4cc51b283b08d11
Huy Thai <huythaia123@gmail.com>
2024-01-04 22:41:50 +0700

    change to server listen

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import 'dotenv/config'
+import { createServer } from 'http'
 import app from './app'
 
 const PORT = process.env.PORT
+const server = createServer(app)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
   console.log(`Root path ::`, process.cwd())
 })
