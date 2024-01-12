d4e564595257639620b47125bdf34e087c94afb5
huy thai <huythaia123@gmail.com>
2024-01-12 17:27:39 +0700

    Update pickKeys and omitKeys function parameter name for clarity

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,12 +2,12 @@ import _ from 'lodash'
 import path from 'path'
 
 // lodash
-export const pickKeys = (obj = {}, arr = []) => {
-  return _.pick(obj, arr)
+export const pickKeys = (obj = {}, keys = []) => {
+  return _.pick(obj, keys)
 }
 
-export const omitKeys = (obj = {}, arr = []) => {
-  return _.omit(obj, arr)
+export const omitKeys = (obj = {}, keys = []) => {
+  return _.omit(obj, keys)
 }
 
 // get path meme image
