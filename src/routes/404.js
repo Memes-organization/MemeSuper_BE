7cb3df7738423f926cf8d8e57ef3608f969b44d7
huy thai <huythaia123@gmail.com>
2024-01-26 17:30:03 +0700

    return route details not found

--- src/routes/404.js
+++ src/routes/404.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import { ReasonPhrases } from 'http-status-codes'
 import { NotFoundException } from 'src/helpers/ErrorResponse'
 
 const router = express.Router()
 
 router.use((req, res, next) => {
-  return next(new NotFoundException(`URL ${req.url} ${ReasonPhrases.NOT_FOUND}`))
+  return next(new NotFoundException(`URL Not Found: ${req.url} `))
 })
 
 export default router
