103f26240caaf2ec363f9e08df9796cd93319f43
huy thai <huythaia123@gmail.com>
2024-01-28 23:00:12 +0700

    validation query string get list meme

--- src/middlewares/validationMw.js
+++ src/middlewares/validationMw.js
@@ -1,12 +1,5 @@
 import { ValidationException } from 'src/helpers/ErrorResponse'
 
-const Joi = require('joi')
-
-const memeListQuerySchema = Joi.object({
-  page: Joi.number().integer().min(1),
-  limit: Joi.number().integer().min(1).max(100),
-})
-
 export const validationQuery = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.query, { abortEarly: false })
